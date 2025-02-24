9a5ecedb85ee810dc58f88516a8e5e0c312e875a
Hasan Mostafa <hasanalimostafa1@gmail.com>
2025-02-25 02:25:54 +0300

    Add human choice logic
    Create the getHumanChoice function which takes
    user input and returns it, if it is one of the valid choices,
    otherwise inform the user that this is an invalid input.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
 function getComputerChoice() {
-
+    
     let randomNumber = Math.floor(Math.random() * 3) + 1;
     switch(randomNumber) {
         case 1:
@@ -9,5 +9,16 @@ function getComputerChoice() {
             return 'paper';
         case 3:
             return 'scissors';
+    }
+}
+
+function getHumanChoice() {
+
+    let userChoice = prompt('What do you choose? rock,paper or scissors?');
+    userChoice = userChoice.toLowerCase();
+    if(userChoice === 'rock' || userChoice === 'paper' || userChoice === 'scissors') {
+        return userChoice;
+    } else {
+        alert('Invalid input!');
     }
 }
