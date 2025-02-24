4c595396ac638de1cf437cd05943a20933eb40c8
Hasan Mostafa <hasanalimostafa1@gmail.com>
2025-02-25 01:53:08 +0300

    Add computer choice logic
    Create the getComputerChoice function that randomly
    returns “rock”, “paper” or “scissors”.
    
    To achive this functionality generate a random number
    between 1 and 3 inclusively,use the
    math.random builtin function(which returns a random number
    that’s greater than or equal to 0 and less than 1)
    with the math.floor function and add 1 to make
    the random range between 1 and 3 inclusively.
    
    Finally return the string according to the random number.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-console.log('Hello World!');
+
+function getComputerChoice() {
+
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    switch(randomNumber) {
+        case 1:
+            return 'rock';
+        case 2:
+            return 'paper';
+        case 3:
+            return 'scissors';
+    }
+}
