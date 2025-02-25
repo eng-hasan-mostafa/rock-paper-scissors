fe3140a4cec7f0baadb8ed23e07c17d1c728a56a
Hasan Mostafa <hasanalimostafa1@gmail.com>
2025-02-25 04:26:26 +0300

    Add the logic to play a single round
    Create the playRound function which takes the human
    and computer player choices as arguments, plays a single round,
    increments the round winner’s score and logs a winner announcement.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 
+let computerScore  = 0;
+let humanScore     = 0;
+
 function getComputerChoice() {
-    
+
     let randomNumber = Math.floor(Math.random() * 3) + 1;
     switch(randomNumber) {
         case 1:
@@ -22,3 +25,61 @@ function getHumanChoice() {
         alert('Invalid input!');
     }
 }
+
+function playRound(humanChoice, computerChoice) {
+
+    if(humanChoice === 'rock') {
+
+        switch(computerChoice) {
+            case 'paper':
+                console.log('You lose! Paper beats Rock');
+                computerScore++;
+                break;
+            case 'scissors':
+                console.log('You win! Rock beats Scissors');
+                humanScore++;
+                break;
+            case 'rock':
+                console.log('It\'s a draw!');
+                break;
+        }
+    }else if(humanChoice === 'paper') {
+        
+        switch(computerChoice) {
+            case 'rock':
+                console.log('You win! Paper beats Rock');
+                humanScore++;
+                break;
+            case 'scissors':
+                console.log('You lose! Scissors beats Paper');
+                computerScore++;
+                break;
+            case 'paper':
+                console.log('It\'s a draw!');
+                break;
+        }
+    }else if(humanChoice === 'scissors') {
+
+        switch(computerChoice) {
+            case 'rock':
+                console.log('You lose! Rock beats Scissors');
+                computerScore++;
+                break;
+            case 'paper':
+                console.log('You win! Scissors beats Paper');
+                humanScore++;
+                break;
+            case 'scissors':
+                console.log('It\'s a draw!');
+                break;
+        }
+    }
+}
+
+
+const humanSelection    = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+console.log('human score', humanScore);
+console.log('computer score', computerScore);
